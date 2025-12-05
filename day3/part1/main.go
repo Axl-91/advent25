c07a0d2734aed10e4ac34daaba0ee39befb650b3
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-12-05 03:21:26 -0300

    Finished part 1 on day3

--- day3/part1/main.go
+++ day3/part1/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func getMaxJoltage(joltageString string) int {
+	first := -1
+	firstIdx := -1
+	second := -1
+
+	for i := 0; i < len(joltageString)-1; i++ {
+		intChr := int(joltageString[i] - '0')
+
+		if intChr > first {
+			first = intChr
+			firstIdx = i
+		}
+	}
+
+	for i := firstIdx + 1; i < len(joltageString); i++ {
+		intChr := int(joltageString[i] - '0')
+		if intChr > second {
+			second = intChr
+		}
+	}
+
+	return (first*10 + second)
+}
 
 func main() {
-	fmt.Println("Run Part1")
+	result := 0
+
+	data, err := os.Open("input")
+	if err != nil {
+		fmt.Println("There was an unexpected error")
+		log.Fatal(err)
+		return
+	}
+	defer data.Close()
+
+	scanner := bufio.NewScanner(data)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		result += getMaxJoltage(line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(result)
 }
