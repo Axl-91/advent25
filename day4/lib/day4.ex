786cb7e8807b2786d61646dca233ac0436a36a25
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-12-06 17:35:54 -0300

    Completed day4

--- day4/lib/day4.ex
+++ day4/lib/day4.ex
@@ -38,13 +38,16 @@ defmodule Day4 do
     end
   end
 
+  def create_matrix(file) do
+    file
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.graphemes/1)
+  end
+
   def part1 do
     file = File.read!("input")
 
-    matrix =
-      file
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.graphemes/1)
+    matrix = create_matrix(file)
 
     rows = length(matrix) - 1
     cols = length(List.first(matrix)) - 1
@@ -55,5 +58,48 @@ defmodule Day4 do
       end
     end
     |> Enum.sum_by(fn a -> Enum.sum(a) end)
+  end
+
+  def run_reductions_from_matrix(matrix, total_removed) do
+    rows = length(matrix) - 1
+    cols = length(List.first(matrix)) - 1
+
+    removed =
+      for i <- 0..rows do
+        for j <- 0..cols do
+          can_be_accessed?(matrix, i, j)
+        end
+      end
+
+    amount_removed = removed |> Enum.sum_by(fn a -> Enum.sum(a) end)
+
+    if amount_removed == 0 do
+      total_removed
+    else
+      new_matrix =
+        matrix
+        |> Enum.with_index()
+        |> Enum.map(fn {row, row_idx} ->
+          row
+          |> Enum.with_index()
+          |> Enum.map(fn {val, val_idx} ->
+            if val == "@" and Enum.at(removed, row_idx) |> Enum.at(val_idx) == 1 do
+              "."
+            else
+              val
+            end
+          end)
+        end)
+
+      run_reductions_from_matrix(new_matrix, amount_removed + total_removed)
+    end
+  end
+
+  def part2 do
+    file = File.read!("input")
+
+    matrix = create_matrix(file)
+
+    run_reductions_from_matrix(matrix, 0)
   end
 end
