0bfb67ad2da093c21c9d42312c5a997d580add10
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-12-12 02:29:07 -0300

    Fix input error and added first iteration with wrong result

--- day8/part1.rb
+++ day8/part1.rb
@@ -1,18 +1,60 @@
 def calculate_distance(x_str, y_str)
-  x_point = x_str.split(',').map(&:to_f)
-  y_point = y_str.split(',').map(&:to_f)
+  x_point = x_str.split(',').map(&:to_i)
+  y_point = y_str.split(',').map(&:to_i)
 
   Math.sqrt(
     (y_point[0] - x_point[0])**2 +
     (y_point[1] - x_point[1])**2 +
-    (y_point[3] - x_point[3])**2
+    (y_point[2] - x_point[2])**2
   )
 end
 
 def main
-  points = File.readlines('input_test', chomp: true)
+  result = 1
+  circuit = []
+  distances = []
+  points = File.readlines('input', chomp: true)
 
-  points.each { |point| puts point }
+  (0..points.size - 2).each do |i|
+    main_point = points[i]
+    points[i + 1..].each do |point|
+      distance = calculate_distance(main_point, point)
+      distances.push([[main_point, point], distance])
+    end
+  end
+
+  distances = distances.sort_by { |list| list[1] }
+
+  (0..1000).each do |i|
+    points = distances[i][0]
+
+    pos_left = circuit.find_index { |junction| junction.any?(points[0]) }
+    pos_right = circuit.find_index { |junction| junction.any?(points[1]) }
+
+    if pos_left && pos_right && pos_left != pos_right
+      left_in_right = circuit[pos_left].any?(points[1])
+      right_in_left = circuit[pos_right].any?(points[0])
+
+      if !left_in_right
+        circuit[pos_left].push(points[1])
+      elsif !right_in_left
+        circuit[pos_right].push(points[0])
+      end
+
+    elsif pos_left && !pos_right
+      circuit[pos_left].push(points[1])
+    elsif pos_right && !pos_left
+      circuit[pos_right].push(points[0])
+    elsif !pos_right && !pos_left
+      circuit.push([points[0], points[1]])
+    end
+  end
+
+  circuit = circuit.map(&:count).sort.reverse
+
+  (0..2).each { |i| result *= circuit[i] }
+
+  puts result
 end
 
 main
