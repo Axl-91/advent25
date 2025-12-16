d7ef5a7eddd12e514cc327585545f248cb1ee3ee
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-12-16 18:05:43 -0300

    Finished part1 on day12

--- day12/part1.jl
+++ day12/part1.jl
@@ -1,4 +1,37 @@
-using LinearAlgebra
+function parse_input(input_name)
+    input_matrixes = Matrix{Int}[]
+    matrix_rows = Vector{Int}[]
+    dims = Vector{Int}[]
+    values = Vector{Int}[]
+
+    for line in eachline(input_name)
+        line = strip(line)
+
+        if (contains(line, 'x'))
+            left, right = split(line, ":")
+
+            dim = parse.(Int, split(left, "x"))
+            value = parse.(Int, split(strip(right)))
+
+            push!(dims, dim)
+            push!(values, value)
+        end
+
+        if isempty(line) || endswith(line, ":")
+            if !isempty(matrix_rows)
+                M = reduce(vcat, transpose.(matrix_rows))
+                push!(input_matrixes, M)
+                empty!(matrix_rows)
+            end
+            continue
+        end
+
+        matrix_row = [c == '#' ? 1 : 0 for c in line]
+        push!(matrix_rows, matrix_row)
+    end
+
+    return input_matrixes, dims, values
+end
 
 function rotations(A)
     rots = Matrix{Int}[]
@@ -52,8 +85,8 @@ function can_place_in_board(board, pieces, cols, rows, idx)
 
     for R in rots
         h, w = size(R)
-        for r in 1:(rows - h + 1),
-            c in 1:(cols - w + 1)
+        for r in 1:(rows-h+1),
+            c in 1:(cols-w+1)
 
             if can_place(board, R, r, c)
                 place!(board, R, r, c)
@@ -83,3 +116,22 @@ function part1(pieces, cols, rows)
 
     return can_place_in_board(board, pieces, cols, rows, 1)
 end
+
+function main()
+    total = 0
+    matrixes, matrix_dimensions, repeats = parse_input("input")
+
+    for i in eachindex(matrix_dimensions)
+        matrix_value_vector = []
+        for j in eachindex(repeats[i])
+            push!(matrix_value_vector, (matrixes[j], repeats[i][j]))
+        end
+        if part1(matrix_value_vector, matrix_dimensions[i][1], matrix_dimensions[i][2])
+            total += 1
+        end
+    end
+
+    println("Total: ", total)
+end
+
+main()
